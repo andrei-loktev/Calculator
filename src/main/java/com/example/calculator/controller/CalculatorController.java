6da8c9f715b4b28fcaad45591adfd744cfab946b
andrei-loktev <119010210+andrei-loktev@users.noreply.github.com>
2023-05-07 00:11:24 +0300

    дз выполнено

---
+++ src/main/java/com/example/calculator/controller/CalculatorController.java
@@ -0,0 +1,48 @@
+package com.example.calculator.controller;
+
+import com.example.calculator.service.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    private final Service service;
+
+    public CalculatorController(Service service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public String greating() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @GetMapping("/plus")
+    public String plus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return String.format("%s + %s = %s", a, b, service.plus(a, b));
+    }
+
+    @GetMapping("/minus")
+    public String minus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return String.format("%s - %s = %s", a, b, service.minus(a, b));
+    }
+
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        return String.format("%s * %s = %s", a, b, service.multiply(a, b));
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+        if (b == 0) {
+            return "на ноль делить нельзя";
+        }
+        return String.format("%s / %s = %s", a, b, service.divide(a, b));
+    }
+}
+
+
